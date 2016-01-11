450cef27e4de9824e517ff74a13077af8220e5e1
Matt Weeks <m_weeks@hotmail.co.uk>
2016-01-11 02:08:40 +0000

    Disconnect all connections when creating database automatically. [Review suggestion]

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,6 +6,13 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'tournament'
+  AND pid <> pg_backend_pid();
+  
+\c vagrant;
+
 DROP DATABASE IF EXISTS tournament;
 
 CREATE DATABASE tournament;
